dc9e282ea9bb4aceab0df0245bc00cb4e65c50f1
Wehan Himsara <wehanhimsara@gmail.com>
2025-05-30 13:51:58 +0530

    Your message about the changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -74,7 +74,7 @@ const HeroSection = () => {
 
         {/* Animated Background Elements */}
         <div className="absolute inset-0">
-          <div className="absolute top-20 left-10 w-64 h-64 bg-white/5 rounded-full blur-3xl animate-float"></div>
+          <div className="absolute top-20  left-10 w-64 h-64 bg-white/5 rounded-full blur-3xl animate-float"></div>
           <div
             className="absolute bottom-20 right-10 w-96 h-96 bg-tezzeract-blue/10 rounded-full blur-3xl animate-float"
             style={{ animationDelay: "3s" }}
